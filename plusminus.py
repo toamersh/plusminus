49c152c693b6959682f7aea8c2b27e60c5bfa6c6
toamersh <toma@ptd.net>
2022-10-17 17:15:43 -0400

    Add files via upload

--- plusminus.py
+++ plusminus.py
@@ -4,16 +4,19 @@ app = Flask(__name__)
 
 from flask import jsonify
 
-@app.route('/plusminus3/<int:number>')
-def addSubtractParam(number):
-	number = int(number)
-	return jsonify({'plus': number+3,'minus': number-3})
+@app.route('/plusminus', methods = ['GET'])
+def addSubtractParam():
+	try:
+		origin = int(request.args.get('origin', None))
+		modifier = int(request.args.get('modifier', None))
+		return jsonify({'plus': origin+modifier,'minus': origin-modifier})
+	except TypeError:
+		return ('An error has occurred.  Please check your query parameters.  Example:  http://localhost:1234/plusminus?origin=56&modifier=9')
 
 @app.route('/plusminus', methods = ['POST'])
 def addSubtractJson():
 	content = request.json
-	pp.pprint(content)
-	return jsonify({'plus': content["base"]+content["number"],'minus': content["base"]-content["number"]})
+	return jsonify({'plus': content["origin"]+content["modifier"],'minus': content["origin"]-content["modifier"]})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1234)
--- plusminusClient.py
+++ plusminusClient.py
@@ -1,7 +1,12 @@
 import requests
 import json
+import sys
 
-response = requests.post('http://localhost:1234/plusminus', json = {"base":10, "number":4})
-if response.ok:
-	responseJson = json.loads(response.text)
-	print (responseJson)
+def main(argv):
+	response = requests.post('http://localhost:1234/plusminus', json = {"origin":int(argv[1]), "modifier":int(argv[2])})
+	if response.ok:
+		responseJson = json.loads(response.text)
+		print (responseJson)
+
+if __name__ == "__main__":
+   main(sys.argv)
